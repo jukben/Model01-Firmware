dcb1196162bf9f12e63fccfc199b46948c0ece8e
Jakub Benes <jukben@gmail.com>
2018-08-06 17:50:46 +0200

    feat: added LEDCustomPalette plugin :)

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -31,17 +31,8 @@
 // when the keyboard is connected to a computer (or that computer is powered on)
 #include "Kaleidoscope-LEDEffect-BootGreeting.h"
 
-// Support for LED modes that set all LEDs to a single color
-#include "Kaleidoscope-LEDEffect-SolidColor.h"
-
-// Support for an LED mode that makes all the LEDs 'breathe'
-#include "Kaleidoscope-LEDEffect-Breathe.h"
-
 // Support for LED modes that pulse the keyboard's LED in a rainbow pattern
 #include "Kaleidoscope-LEDEffect-Rainbow.h"
-
-// Support for an LED mode that lights up the keys as you press them
-#include "Kaleidoscope-LED-Stalker.h"
 
 // Support for Keyboardio's internal keyboard testing mode
 #include "Kaleidoscope-Model01-TestMode.h"
@@ -54,6 +45,18 @@
 
 // Support for USB quirks, like changing the key state report protocol
 #include "Kaleidoscope-USB-Quirks.h"
+
+// Support Serial Comunication
+#include "Kaleidoscope-Focus.h"
+
+// Support EEPROM-Settings (requirement for LED Palette Theme)
+#include "Kaleidoscope-EEPROM-Settings.h"
+
+// Support LED Palette Theme (requirement for LEDCustomPallette)
+#include "Kaleidoscope-LED-Palette-Theme.h"
+
+// Support custom keyboard via LED Custom Palette
+#include "Kaleidoscope-LEDCustomPalette.h"
 
 /** This 'enum' is a list of all the macros used by the Model 01's firmware
   * The names aren't particularly important. What is important is that each
@@ -230,18 +233,6 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState)
   return MACRO_NONE;
 }
 
-// These 'solid' color effect definitions define a rainbow of
-// LED color modes calibrated to draw 500mA or less on the
-// Keyboardio Model 01.
-
-static kaleidoscope::LEDSolidColor solidRed(160, 0, 0);
-static kaleidoscope::LEDSolidColor solidOrange(140, 70, 0);
-static kaleidoscope::LEDSolidColor solidYellow(130, 100, 0);
-static kaleidoscope::LEDSolidColor solidGreen(0, 160, 0);
-static kaleidoscope::LEDSolidColor solidBlue(0, 70, 130);
-static kaleidoscope::LEDSolidColor solidIndigo(0, 0, 170);
-static kaleidoscope::LEDSolidColor solidViolet(130, 0, 120);
-
 /** toggleLedsOnSuspendResume toggles the LEDs off when the host goes to sleep,
  * and turns them back on when it wakes up.
  */
@@ -318,22 +309,9 @@ KALEIDOSCOPE_INIT_PLUGINS(
     // We start with the LED effect that turns off all the LEDs.
     LEDOff,
 
-    // The rainbow effect changes the color of all of the keyboard's keys at the same time
-    // running through all the colors of the rainbow.
-    LEDRainbowEffect,
-
     // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
     // and slowly moves the rainbow across your keyboard
     LEDRainbowWaveEffect,
-
-    // These static effects turn your keyboard's LEDs a variety of colors
-    solidRed, solidOrange, solidYellow, solidGreen, solidBlue, solidIndigo, solidViolet,
-
-    // The breathe effect slowly pulses all of the LEDs on your keyboard
-    LEDBreatheEffect,
-
-    // The stalker effect lights up the keys you've pressed recently
-    StalkerEffect,
 
     // The numpad plugin is responsible for lighting up the 'numpad' mode
     // with a custom LED effect
@@ -353,6 +331,14 @@ KALEIDOSCOPE_INIT_PLUGINS(
     // actions - a bit like Macros, but triggered by pressing multiple keys at the
     // same time.
     MagicCombo,
+
+    Focus,
+
+    EEPROMSettings,
+
+    LEDPaletteTheme,
+
+    LEDCustomPalette,
 
     // The USBQuirks plugin lets you do some things with USB that we aren't
     // comfortable - or able - to do automatically, but can be useful
@@ -381,18 +367,18 @@ void setup()
 
   // We set the brightness of the rainbow effects to 150 (on a scale of 0-255)
   // This draws more than 500mA, but looks much nicer than a dimmer effect
-  LEDRainbowEffect.brightness(150);
   LEDRainbowWaveEffect.brightness(150);
-
-  // The LED Stalker mode has a few effects. The one we like is
-  // called 'BlazingTrail'. For details on other options,
-  // see https://github.com/keyboardio/Kaleidoscope-LED-Stalker
-  StalkerEffect.variant = STALKER(BlazingTrail);
 
   // We want to make sure that the firmware starts with LED effects off
   // This avoids over-taxing devices that don't have a lot of power to share
   // with USB devices
   LEDOff.activate();
+
+  Focus.addHook(FOCUS_HOOK_HELP);
+  Focus.addHook(FOCUS_HOOK_VERSION);
+  Focus.addHook(FOCUS_HOOK_LEDCONTROL);
+  Focus.addHook(FOCUS_HOOK_LEDPALETTETHEME);
+  Focus.addHook(FOCUS_HOOK_LCP);
 }
 
 /** loop is the second of the standard Arduino sketch functions.
@@ -406,4 +392,3 @@ void loop()
 {
   Kaleidoscope.loop();
 }
-
